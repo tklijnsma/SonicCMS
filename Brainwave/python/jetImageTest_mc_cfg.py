f4688defa76fc89614194965fbfdad751b1550df
Thomas Klijnsma <klijnsma@fnal.gov>
2020-05-06 22:56:02 -0500

    added outputfile and moved top_classes txt

--- Brainwave/python/jetImageTest_mc_cfg.py
+++ Brainwave/python/jetImageTest_mc_cfg.py
@@ -11,6 +11,7 @@ options.register("timeout", 30, VarParsing.multiplicity.singleton, VarParsing.va
 options.register("params", "", VarParsing.multiplicity.singleton, VarParsing.varType.string)
 options.register("threads", 1, VarParsing.multiplicity.singleton, VarParsing.varType.int)
 options.register("streams", 0, VarParsing.multiplicity.singleton, VarParsing.varType.int)
+options.outputFile = 'output.txt'
 options.inputFiles = 'file:../../Core/data/store_mc_RunIISpring18MiniAOD_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph_MINIAODSIM_100X_upgrade2018_realistic_v10-v1_30000_24A0230C-B530-E811-ADE3-14187741120B.root',
 options.parseArguments()
 
@@ -59,9 +60,9 @@ if len(options.inputFiles)>0: process.source.fileNames = options.inputFiles
 process.jetImageProducer = cms.EDProducer(prodname,
     JetTag = cms.InputTag('slimmedJetsAK8'),
     topN = cms.uint32(5),
-#    imageList = cms.string("../../Core/data/imagenet_classes.txt"),
-    imageList = cms.string("../../Core/data/top_classes.txt"),
+    imageList = cms.string("top_classes.txt"),
     Client = Client,
+    outFile = cms.string(options.outputFile)
 )
 
 # Let it run
